26c6d1ab166432e737e6fab982308e17f1fcc9ee
cf2141 <cf2141@tgndomains.com>
2018-01-12 17:10:10 -0500

    added imports

--- src/main/java/com/acme/CFsftpd01.java
+++ src/main/java/com/acme/CFsftpd01.java
@@ -1,5 +1,10 @@
 package com.acme;
 
+import java.io.FileInputStream;
+import java.io.File;
+import java.util.Properties;
+//import org.apache.log4j.Logger;
+
 public class CFsftpd01 implements Runnable {
   //- - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - -
   public CFsftpd01() {
